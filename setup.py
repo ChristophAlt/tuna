d5ed623cf22b7c29f39d0c2804440026fc2dd8a5
Christoph Alt <christoph.alt@dfki.de>
2019-01-27 21:44:22 +0100

    Updated version, name and packages

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
-from package_settings import NAME, VERSION, PACKAGES, DESCRIPTION
 from setuptools import setup
 
 
 setup(
-    name=NAME,
-    version=VERSION,
-    long_description=DESCRIPTION,
+    name="tuna",
+    version="0.0.1",
+    long_description="Hyperparameter tuning for AllenNLP, powered by Ray.",
     author="Christoph Alt",
     author_email="christoph.alt@posteo.de",
-    packages=PACKAGES,
     include_package_data=True,
     install_requires=["ray==0.6.2"],
     package_data={"": ["*.*"]},
